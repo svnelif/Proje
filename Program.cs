87930d419bab62ed2ad634ca0d3e6de50914de8d
Elif Ataseven <22010310015@ogrenci.bartin.edu.tr>
2025-07-23 17:37:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ML;
+using OriginML.DataStructures.ErrorSuggestion;
+using OriginML.ErrorSuggestions;
+
+
+
+#region ErrorCorrections
+
+MLMain mLMain = new MLMain();
+mLMain.Start();
+Console.WriteLine("Model Kaydedildi... Şimdi yükleniyor...");
+
+var mlContext = new MLContext(seed: 1);
+ITransformer trainedModel = mlContext.Model.Load(MLMain.ModelPath, out var modelSchema);
+
+var issue = new OriginError() { ID = "Any-ID", Description = @"Error occured in r_add_implicit_rec_with_ascode. Error message: Ascode already exists: 44103. See model logs for detailed information." };
+var predEngine = mlContext.Model.CreatePredictionEngine<OriginError, OriginErrorPrediction>(trainedModel);
+
+var prediction = predEngine.Predict(issue);
+Console.WriteLine("");
+Console.WriteLine($"=============== Error Message ===============");
+Console.WriteLine(issue.Description);
+
+Console.WriteLine("");
+Console.WriteLine($"=============== Prediction ===============");
+Console.WriteLine(prediction.Area);
+Console.ReadLine();
+
+#endregion
+
+
+Console.ReadLine();
